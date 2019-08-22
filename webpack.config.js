a923fa747e3473a619b5e7e2d78e5b6129a332c7
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-08-22 14:58:59 +0800

    Update babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,13 @@
-var webpack = require('webpack');
-var path = require('path');
-
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    filename: 'dist/bookingform.js',
-    sourceMapFilename: '/dist/bookingform-bundle.map'
-  },
-  devtool: '#source-map',
   module: {
-    loaders: [
+    rules: [
       {
-        loader: 'babel-loader',
-        exclude: '/(node_modules)/',
-        query: {
-          presets: ['react', 'es2015']
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
       }
     ]
-  } 
-}
+  }
+};
