1c4bd710f4bd5dde0e1bb16beecb7ee0ec2eb492
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-22 22:25:11 +0800

    Remove unused style

--- src/Components/BookingForm/NumPax/PaxSection.js
+++ src/Components/BookingForm/NumPax/PaxSection.js
@@ -6,39 +6,6 @@ import { Consumer, BookingContext } from '../Context';
 const Title = styled.p`
   margin: 0 auto;
 `
-const List = styled.ul`
-  margin: 5px 0;
-  padding: 0;
-  list-style: none;
-  display:grid;
-  grid-template-columns:repeat(auto-fill, 40px);
-  grid-auto-rows: 40px;
-  grid-gap: 3px 3px;
-`
-const Item = styled.li`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 2px;
-  border: 1px solid gray;
-  background-color: white;
-  opacity: .7;
-
-  :hover {
-    cursor: pointer;
-    color: #484848;   
-  }
-
-  ${props => props.selected && `
-    border-color: rgba(46, 204, 113,1.0);
-    background-color: rgba(46, 204, 113,1.0);
-    color: white; 
-    opacity: 1.0;
-    :hover {
-      color: white;
-    }
-  `}
-`
 
 const Section = styled.section`
   padding: 5px 0;
