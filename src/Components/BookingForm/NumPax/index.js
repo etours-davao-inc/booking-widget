e161f37bef918acfcc39d13baf6858a1ce4c4051
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-23 19:29:50 +0800

    Refactor and Improve styling of pax

--- src/Components/BookingForm/NumPax/index.js
+++ src/Components/BookingForm/NumPax/index.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PaxSection from './PaxSection';
-
 import { ButtonGroup, Button } from '../../Buttons';
-import ScrolledYWrapper from '../ScrolledYWrapper';
-
-import { Consumer } from '../Context';
+import { BookingContext } from '../Context';
 
 export default () => {
+  const { actions } = useContext(BookingContext);
   return (
-    <React.Fragment>
-      <ScrolledYWrapper>
-        <p className="m-0 py-3 font-weight-bold">Select number of persons</p>
-        <PaxSection title="Adults" type="adults"></PaxSection>
-        <PaxSection title="Kids 0-2 yrs old" type="kid02"></PaxSection>
-        <PaxSection title="Kids 3-5 yrs old" type="kid35"></PaxSection>
-        <PaxSection title="Kids 6-11 yrs old" type="kid611"></PaxSection>
-      </ScrolledYWrapper>
-      <Consumer>
-        { ({actions}) => {
-          return (
-            <ButtonGroup>
-              <Button next onClick={()=>actions.step('+')}>Select tour date</Button>
-            </ButtonGroup>
-          )
-        } }
-      </Consumer>  
-
-    </React.Fragment>
+    <>
+      <p className="m-0 py-3 font-weight-bold">Select number of persons</p>
+      <div style={styles.section}>
+        <PaxSection title="Adult" type="adults"></PaxSection>
+        <PaxSection title="Child (0-2 yrs)" type="kid02"></PaxSection>
+        <PaxSection title="Child (3-5 yrs)" type="kid35"></PaxSection>
+        <PaxSection title="Child (6-11 yrs)" type="kid611"></PaxSection>
+      </div>
+      <ButtonGroup>
+        <Button next onClick={() => actions.step('+')}>Select tour date</Button>
+      </ButtonGroup>
+    </>
   )
 };
+
+const styles = {
+  section: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap: '15px'
+
+  }
+}
+
