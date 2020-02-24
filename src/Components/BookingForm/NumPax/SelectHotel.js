8e9a722cb8a62ae77e306cd56c103a2f13bd3a86
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-24 19:49:10 +0800

    Get hotels from BookingContext, improve styling

--- src/Components/BookingForm/NumPax/SelectHotel.js
+++ src/Components/BookingForm/NumPax/SelectHotel.js
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 import Select from './SelectStyles';
 
-const SelectHotel = () => {
+const SelectHotel = ({ hotels }) => {
   return (
     <div style={{}}>
       <p className="p-0 m-0">Select Accommodation</p>
-      <Select style={{ width: '100%' }}>
-        <option>
-          Tour Only
-      </option>
-        <option>
-          Marco Polo hotel at 2500/night
-      </option>
+      <Select style={styles.select}>
+        <option value={'tour-only'} key={0}>Tour Only</option>
+        {hotels.map(({code, name, price}) => <option key={code} value={code}>{`${name.toLowerCase()} at Php ${price}`}</option>)}
       </Select>
     </div>
   )
 }
 
-const style = {
-
+const styles = {
+  select: {
+    width: '100%',
+    textTransform: 'capitalize'
+  }
 }
 
 export default SelectHotel;
