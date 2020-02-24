2a466b679cf37fbec7d1c490320a904995925ece
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-24 18:04:40 +0800

    Extract and import DatePicker

--- src/Components/BookingForm/NumPax/SelectDate.js
+++ src/Components/BookingForm/NumPax/SelectDate.js
@@ -1,37 +1,17 @@
 import React, { useState } from 'react';
 import Select from './SelectStyles';
+import DatePicker from './DatePicker'
 
 export default (props) => {
-
   return (
     <>
       <div style={styles.dates}>
         <p className="p-0 m-0">Arrival date</p>
-        <div style={styles.datepicker}>
-          <Select>
-            <option>January</option>
-          </Select>
-          <Select>
-            <option>31</option>
-          </Select>
-          <Select>
-            <option>2027</option>
-          </Select>
-        </div>
+        <DatePicker minDate={new Date(2020, 1, 28)}/>
       </div>
       <div style={styles.dates, { marginBottom: '25px' }}>
         <p className="p-0 m-0">Departure date</p>
-        <div style={styles.datepicker}>
-          <Select>
-            <option>January</option>
-          </Select>
-          <Select>
-            <option>31</option>
-          </Select>
-          <Select>
-            <option>2027</option>
-          </Select>
-        </div>
+        <DatePicker />
       </div>
     </>
   )
@@ -42,9 +22,4 @@ const styles = {
     marginBottom: '15px'
 
   },
-  datepicker: {
-    display: 'grid',
-    gridTemplateColumns: '2fr 1fr 2fr',
-    gridGap: '15px',
-  }
 }
