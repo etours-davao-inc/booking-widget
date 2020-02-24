cbaf38b39e7aa8ba234a170f0b98fb951a4096da
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-25 01:47:19 +0800

    Refactor, make context function component

--- src/Components/BookingForm/Context/index.js
+++ src/Components/BookingForm/Context/index.js
@@ -1,167 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import isEmail from 'validator/lib/isEmail';
 
 import { Calculate } from 'price-compute-js';
 import postReservation  from './postReservation';
 
-import { initDayRangeValues, setDayRange, getStartDate, setOptionPaymentDate } from 'tour-dates-utility';
-import { DayTour, MultiDay, MultiDayWithHotel } from './Modules';
+import { setDayRange, setOptionPaymentDate } from 'tour-dates-utility';
 
 export const BookingContext = React.createContext();
 
-export class Provider extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: props.data,
-      currentStep: 0,
-      currentModule: '',
-      applicableModules: '',
-      RFValid: false,
-      termsAccepted: false,
-      package: {
-        name: props.data.name,
-        code: props.data.code,
-        slug: props.data.slug,
-        type: props.data.type,
-        duration: props.data.duration,
-        durationText: props.data.duration_text,
-        info: props.data.info,
-        responsibilities: props.data.responsibilites,
-        terms: props.data.terms
-      },
-      userInput: {
-        inquiryDate: new Date(),
-        adults: props.data.price.adults[0],
-        kid02: [0, 0],
-        kid35: [0, 0],
-        kid611: [0, 0],
-        name: "",
-        email: "",
-        contact: "",
-        remarks: ""
-      },
-      calculations: {},
-    }
+export const Provider = ({data, children}) => {
+  const { name, code, slug, type, duration, duration_text, info, responsibilities, terms } = data
+  const [state, setState] = useState({ data: data, RFValid: false, termsAccepted: false})
+  const [tourPackage] = useState({name, code, slug, type, duration, duration_text, info, responsibilities, terms })
+  const [userInput, setUserInput] = useState({
+    inquiryDate: new Date(),
+    adults: data.price.adults[0],
+    kid02: [0, 0],
+    kid35: [0, 0],
+    kid611: [0, 0],
+    name: "",
+    email: "",
+    contact: "",
+    remarks: ""
+  })
+  const [calculations, setCalculations] = useState({})
+
+  const onPaxSelect = (payload) => {
+    setUserInput({...userInput,...payload})
   }
 
-  componentWillMount() {
-    new Promise((resolve, reject) => {
-      resolve(this.loadProperModules())
-    }).then((resolve) => {
-      let currentStep = this.state.currentStep
-      this.updateCurrentModule(currentStep, resolve)
+  const onSelectDate = (tourDate) => {
+    setOptionPaymentDate(userInput.inquiryDate, payload).then(optionDate => {
+      setUserInput({...userInput, tourDate, optionDate})
     })
-    this.initializeDates()
-
   }
 
-  componentDidMount() {
-    window.scrollTo(0,0)
+  const onSelectDates = (tourDates) => {
+    setOptionPaymentDate(userInput.inquiryDate, tourDates.from).then(optionDate => {
+      setUserInput({...userInput, tourDates, optionDate})
+    })
   }
 
-  loadProperModules() {
-    let tourType = this.state.data.type
-    let withHotels = this.state.data.hotels !== undefined
-    let applicableModules;
-    if (tourType === 'daytour') {
-      applicableModules = DayTour
-    } else if (tourType === 'multiday' && withHotels) {
-      applicableModules = MultiDayWithHotel
-    } else if (tourType === 'multiday' && !withHotels) {
-      applicableModules = MultiDay
-    }
-    return applicableModules
-  }
-
-  initializeDates() {
-    let userInput = this.state.userInput;
-    let { duration, startday, offsetnights, limitdays } = this.state.data
-    let today = new Date();
-    if (this.state.data.type === "multiday") {
-      let { from, to, days, maxDays, nights, hotelNights } = initDayRangeValues(today, duration, startday, offsetnights, limitdays)
-      userInput.tourDates = { from: from, to: to, days: days, nights: nights, hotelNights: hotelNights }
-      setOptionPaymentDate(today, from)
-      .then(optionDate => {
-        userInput.tourDates.optionDate = optionDate
-      })
-      this.setState({ userInput: userInput, disabledDaysBefore: from, minDays: duration, maxDays: maxDays, isRange: true, })
+  const onSelectHotel = (payload) => {
+    if (payload) {
+      setUserInput({...userInput, hotel:state.data.hotels.find(hotel => hotel.code === payload)})
     } else {
-      userInput.tourDate = getStartDate(today, startday)
-      setOptionPaymentDate(userInput.inquiryDate, userInput.tourDate)
-      .then(optionDate => {
-        userInput.optionDate = optionDate
-      })
-      this.setState({ userInput: userInput, disabledDaysBefore: userInput.tourDate })
+      setUserInput({...userInput, hotel: {code:"", price:0, name:""}})
     }
   }
 
-  step(key) {
-    let currentStep = this.state.currentStep
-    switch (key) {
-      case '+':
-        currentStep++
-        break;
-      case '-':
-        currentStep--
-      default:
-        break;
-    }
-    let applicableModules = this.state.applicableModules
-    this.updateCurrentModule(currentStep, applicableModules)
-  }
-
-  updateCurrentModule(currentStep, applicableModules) {
-    let currentModule = applicableModules[currentStep]
-    if (currentModule == 'Computation') {
-      this.doComputations()
-    }
-    this.setState({
-      currentStep: currentStep,
-      currentModule: currentModule,
-      applicableModules: applicableModules
-    })
-  }
-
-  doComputations() {
-    let calculations = Calculate(this.state.userInput);
-    this.setState({ calculations: calculations })
-  }
-
-  handlePaxClick(paxObj) {
-    let userInput = this.state.userInput
-    let key = Object.keys(paxObj)[0]
-    userInput[key] = paxObj[key]
-    this.setState({ userInput: userInput })
-  }
-
-  clickNewDate(date) {
-    let userInput = this.state.userInput
-    userInput.tourDate = date;
-    setOptionPaymentDate(userInput.inquiryDate, date).then(optionDate => {
-      userInput.optionDate = optionDate;
-    })
-    this.setState({ userInput: userInput })
-  }
-
-  clickDayRange(date) {
-    let updatedState = setDayRange(this.state, date)
-    let today = updatedState.userInput.inquiryDate
-    let from = updatedState.userInput.tourDates.from
-    setOptionPaymentDate(today, from).then(optionDate => {
-      updatedState.userInput.tourDates.optionDate = optionDate;
-    })
-    this.setState(updatedState)
-  }
-
-  hotelClick(payload) {
-    let userInput = this.state.userInput
-    userInput.hotel = payload
-    this.setState({ userInput: userInput })
-  }
-
-  handleRFChange(e) {
+  const handleRFChange = (e) => {
     let userInput = { ...this.state.userInput }
     let termsAccepted = this.state.termsAccepted
     if (e.target.name == "terms") {
@@ -178,77 +67,74 @@ export class Provider extends Component {
     })
   }
 
-  isvalidRF(userInput, termsAccepted) {
-    let name = userInput.name
-    let email = userInput.email
-    if (name != ""
-      && email != ""
-      && isEmail(email)
-      && termsAccepted) {
-      return true
-    } else {
-      return false
-    }
+  const isvalidRF = (userInput, termsAccepted) => {
+    // let name = userInput.name
+    // let email = userInput.email
+    // if (name != ""
+    //   && email != ""
+    //   && isEmail(email)
+    //   && termsAccepted) {
+    //   return true
+    // } else {
+    //   return false
+    // }
   }
 
-  submitBooking = () => {
-    postReservation({
-      input: this.state.userInput,
-      calculations: this.state.calculations,
-      package: this.state.package
-    })
-    this.step('+')
+  const submitBooking = () => {
+    // postReservation({
+    //   input: this.state.userInput,
+    //   calculations: this.state.calculations,
+    //   package: this.state.package
+    // })
+    // this.step('+')
   }
 
-  resetBookingForm = () => {
-    this.setState({
-      currentStep: 0,
-      currentModule: '',
-      RFValid: false,
-      termsAccepted: false,
-      userInput: {
-        inquiryDate: new Date(),
-        adults: this.state.data.price.adults[0],
-        kid02: [0, 0],
-        kid35: [0, 0],
-        kid611: [0, 0],
-        name: "",
-        email: "",
-        contact: "",
-        remarks: ""
-      },
-      calculations: {},
-    })
+  const doComputations = () => {
+    setCalculations(Calculate(userInput))
   }
 
-  render() {
+  const resetBookingForm = () => {
+    // this.setState({
+    //   currentStep: 0,
+    //   currentModule: '',
+    //   RFValid: false,
+    //   termsAccepted: false,
+    //   userInput: {
+    //     inquiryDate: new Date(),
+    //     adults: this.state.data.price.adults[0],
+    //     kid02: [0, 0],
+    //     kid35: [0, 0],
+    //     kid611: [0, 0],
+    //     name: "",
+    //     email: "",
+    //     contact: "",
+    //     remarks: ""
+    //   },
+    //   calculations: {},
+    // })
+  }
     return (
       <BookingContext.Provider value={{
-        data: this.state.data,
-        prices: this.state.data.price,
-        currentStep: this.state.currentStep,
-        currentModule: this.state.currentModule,
-        RFValid: this.state.RFValid,
-        termsAccepted: this.state.termsAccepted,
-        userInput: this.state.userInput,
-        calculations: this.state.calculations,
-        disableDaysBefore: this.state.disabledDaysBefore,
-        isRange: this.state.isRange,
+        data: state.data,
+        prices: state.data.price,
+        RFValid: state.RFValid,
+        termsAccepted: state.termsAccepted,
+        userInput,
+        calculations,
         actions: {
-          step: this.step.bind(this),
-          handlePaxClick: this.handlePaxClick.bind(this),
-          clickNewDate: this.clickNewDate.bind(this),
-          clickDayRange: this.clickDayRange.bind(this),
-          hotelClick: this.hotelClick.bind(this),
-          handleRFChange: this.handleRFChange.bind(this),
-          submitBooking: this.submitBooking.bind(this),
-          resetBookingForm: this.resetBookingForm.bind(this)
+          onPaxSelect,
+          onSelectDate,
+          onSelectDates,
+          onSelectHotel,
+          handleRFChange,
+          submitBooking,
+          resetBookingForm
         }
       }}>
-        {this.props.children}
+        {children}
       </BookingContext.Provider>
     )
-  }
+
 }
 
 export const Consumer = BookingContext.Consumer;
