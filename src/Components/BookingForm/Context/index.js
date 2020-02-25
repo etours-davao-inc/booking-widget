75ce6bcdd57250bb02eaf525ea1859f6b5ee9614
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-25 20:12:52 +0800

    Update initialization of BookingContext

--- src/Components/BookingForm/Context/index.js
+++ src/Components/BookingForm/Context/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { addDays, differenceInCalendarDays } from 'date-fns';
 
 import isEmail from 'validator/lib/isEmail';
 
@@ -10,11 +11,12 @@ import { setDayRange, setOptionPaymentDate } from 'tour-dates-utility';
 export const BookingContext = React.createContext();
 
 export const Provider = ({data, children}) => {
+  const today = new Date();
   const { name, code, slug, type, duration, duration_text, info, responsibilities, terms } = data
-  const [state, setState] = useState({ data: data, RFValid: false, termsAccepted: false})
-  const [tourPackage] = useState({name, code, slug, type, duration, duration_text, info, responsibilities, terms })
-  const [userInput, setUserInput] = useState({
-    inquiryDate: new Date(),
+  const {startday, offsetnights} = data;
+
+  const InitialUserInput = {
+    inquiryDate: today,
     adults: data.price.adults[0],
     kid02: [0, 0],
     kid35: [0, 0],
@@ -22,32 +24,65 @@ export const Provider = ({data, children}) => {
     name: "",
     email: "",
     contact: "",
-    remarks: ""
-  })
-  const [calculations, setCalculations] = useState({})
+    remarks: "",
+  }
+
+  if (type === 'multiday') {
+    const tourDates = {};
+    tourDates.from = addDays(today, startday);
+    setOptionPaymentDate(today, tourDates.from).then(date => {
+      InitialUserInput.optionDate = date;
+    })
+    tourDates.to = addDays(tourDates.from, duration-1);
+    tourDates.days = differenceInCalendarDays(tourDates.to, tourDates.from) + 1;
+    tourDates.nights = tourDates.days - 1;
+    tourDates.hotelNights = tourDates.nights - offsetnights;
+    InitialUserInput.tourDates = tourDates;
+    InitialUserInput.hotel = {code:"", price:0, name:""};
+    InitialUserInput.startDate = tourDates.from
+  } else {
+    setOptionPaymentDate(today, tourDate).then(date => {
+      InitialUserInput.optionDate = date
+      InitialUserInput.tourDate = addDays(today, startday);
+    }) 
+  } 
+
+  const [state, setState] = useState({ data: data, RFValid: false, termsAccepted: false })
+  const [tourPackage] = useState({name, code, slug, type, duration, duration_text, info, responsibilities, terms })
+  const [userInput, setUserInput] = useState(InitialUserInput)
+
+  const [calculations, setCalculations] = useState(Calculate(userInput));
 
   const onPaxSelect = (payload) => {
     setUserInput({...userInput,...payload})
+    doComputations({...userInput,...payload})
   }
 
   const onSelectDate = (tourDate) => {
     setOptionPaymentDate(userInput.inquiryDate, payload).then(optionDate => {
       setUserInput({...userInput, tourDate, optionDate})
+      doComputations({...userInput, tourDate, optionDate})
     })
+    
   }
 
   const onSelectDates = (tourDates) => {
     setOptionPaymentDate(userInput.inquiryDate, tourDates.from).then(optionDate => {
       setUserInput({...userInput, tourDates, optionDate})
+      doComputations({...userInput, tourDates, optionDate})
     })
+   
   }
 
   const onSelectHotel = (payload) => {
     if (payload) {
       setUserInput({...userInput, hotel:state.data.hotels.find(hotel => hotel.code === payload)})
+      doComputations({...userInput, hotel:state.data.hotels.find(hotel => hotel.code === payload)})
     } else {
       setUserInput({...userInput, hotel: {code:"", price:0, name:""}})
+      doComputations({...userInput, hotel: {code:"", price:0, name:""}})
     }
+    
   }
 
   const handleRFChange = (e) => {
@@ -89,8 +124,8 @@ export const Provider = ({data, children}) => {
     // this.step('+')
   }
 
-  const doComputations = () => {
-    setCalculations(Calculate(userInput))
+  const doComputations = (payload) => {
+    if(payload.hotel && payload.tourDates) setCalculations(Calculate(payload))
   }
 
   const resetBookingForm = () => {
