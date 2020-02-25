3d92a034a946bd885f9582b1fcc47b67ade0024e
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-25 18:40:35 +0800

    Refactor and useContext hook

--- src/Components/BookingForm/Computation.js
+++ src/Components/BookingForm/Computation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Currency from 'react-currency-formatter';
 
 import styled from 'styled-components';
@@ -6,7 +6,67 @@ import styled from 'styled-components';
 import { ButtonGroup, Button } from '../Buttons';
 import ScrolledYWrapper from './ScrolledYWrapper';
 
-import { Consumer } from './Context';
+import { BookingContext } from './Context';
+
+
+
+export default () => {
+  const { data, userInput, calculations } = useContext(BookingContext);
+  return (
+
+
+    <ComputationWrapper>
+      <p className="m-0 py-3 font-weight-bold">View summary</p>
+      <dl className="row small">
+        <dt className="col-sm-3">Name:</dt>
+        <dd className="col-sm-9">{data.name}</dd>
+        <dt className="col-sm-3">Duration:</dt>
+        <dd className="col-sm-9">{data.hotels ? `${userInput.tourDates.days} days and ${userInput.tourDates.nights} nights` : data.duration_text}</dd>
+      </dl>
+      <div id="summary_wrapper">
+        <div className="computationGrid">
+          <p></p><p>Price</p><p>Qty</p><p></p>
+        </div>
+        <div className="computationGrid">
+          <p>Adults</p>
+          <p><Currency quantity={calculations.item.adults} currency="PHP" pattern="!##,### " /></p>
+          <p>{userInput.adults[0]}</p>
+          <p><Currency quantity={calculations.total.adults} currency="PHP" pattern="!##,### " /></p>
+        </div>
+        <div className="computationGrid">
+          <p>Kids (0-2 yrs)</p>
+          <p><Currency quantity={calculations.item.kid02} currency="PHP" pattern="!##,### " /></p>
+          <p>{userInput.kid02[0]}</p>
+          <p><Currency quantity={calculations.total.kid02} currency="PHP" pattern="!##,### " /></p>
+        </div>
+        <div className="computationGrid">
+          <p>Kids (3-5 yrs)</p>
+          <p><Currency quantity={calculations.item.kid35} currency="PHP" pattern="!##,### " /></p>
+          <p>{userInput.kid35[0]}</p>
+          <p><Currency quantity={calculations.total.kid35} currency="PHP" pattern="!##,### " /></p>
+        </div>
+        <div className="computationGrid">
+          <p>Kids (6-11 yrs)</p>
+          <p><Currency quantity={calculations.item.kid611} currency="PHP" pattern="!##,### " /></p>
+          <p>{userInput.kid611[0]}</p>
+          <p><Currency quantity={calculations.total.kid611} currency="PHP" pattern="!##,### " /></p>
+        </div>
+        <div className="computationGrid">
+          <p className="computationTotalBalance">Total</p>
+          <p><Currency quantity={calculations.total.total} currency="PHP" pattern="!##,### " /></p>
+        </div>
+        <div className="computationGrid">
+          <p className="computationTotalBalance">Required downpayment to confirm your reservation</p>
+          <p><Currency quantity={calculations.total.downpayment} currency="PHP" pattern="!##,### " /></p>
+        </div>
+        <div className="computationGrid">
+          <p className="computationTotalBalance">Balance to be paid on the first day of your tour</p>
+          <p><Currency quantity={calculations.total.balance} currency="PHP" pattern="!##,### " /></p>
+        </div>
+      </div>
+    </ComputationWrapper>
+  )
+}
 
 const ComputationWrapper = styled.section`
   .computationGrid {
@@ -36,75 +96,3 @@ const ComputationWrapper = styled.section`
     grid-column-end: 4;
   }    
 `
-
-export default () => {
-  return (
-    <Consumer>
-      {({ data, userInput, calculations, actions }) => {
-        return (
-          <React.Fragment>
-            <ScrolledYWrapper>
-              <ComputationWrapper>
-              <p className="m-0 py-3 font-weight-bold">View summary</p>
-                <dl className="row small">
-                  <dt className="col-sm-3">Name:</dt>
-                  <dd className="col-sm-9">{data.name}</dd>
-                  <dt className="col-sm-3">Duration:</dt>
-                  <dd className="col-sm-9">{userInput.hotel ? `${userInput.tourDates.days} days and ${userInput.tourDates.nights} nights`: data.duration_text}</dd>
-                </dl>
-                <div id="summary_wrapper">
-                  <div className="computationGrid">
-                    <p></p><p>Price</p><p>Qty</p><p></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p>Adults</p>
-                    <p><Currency quantity={calculations.item.adults} currency="PHP" pattern="!##,### " /></p>
-                    <p>{userInput.adults[0]}</p>
-                    <p><Currency quantity={calculations.total.adults} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p>Kids (0-2 yrs)</p>
-                    <p><Currency quantity={calculations.item.kid02} currency="PHP" pattern="!##,### " /></p>
-                    <p>{userInput.kid02[0]}</p>
-                    <p><Currency quantity={calculations.total.kid02} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p>Kids (3-5 yrs)</p>
-                    <p><Currency quantity={calculations.item.kid35} currency="PHP" pattern="!##,### " /></p>
-                    <p>{userInput.kid35[0]}</p>
-                    <p><Currency quantity={calculations.total.kid35} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p>Kids (6-11 yrs)</p>
-                    <p><Currency quantity={calculations.item.kid611} currency="PHP" pattern="!##,### " /></p>
-                    <p>{userInput.kid611[0]}</p>
-                    <p><Currency quantity={calculations.total.kid611} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p className="computationTotalBalance">Total</p>
-                    <p><Currency quantity={calculations.total.total} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p className="computationTotalBalance">Required downpayment to confirm your reservation</p>
-                    <p><Currency quantity={calculations.total.downpayment} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                  <div className="computationGrid">
-                    <p className="computationTotalBalance">Balance to be paid on the first day of your tour</p>
-                    <p><Currency quantity={calculations.total.balance} currency="PHP" pattern="!##,### " /></p>
-                  </div>
-                </div>
-              </ComputationWrapper>
-            </ScrolledYWrapper>
-            <ButtonGroup>
-              <Button back className="BookingFormButtons btn-back" onClick={() => actions.step('-')}>Back</Button>
-              <Button next className="BookingFormButtons btn-next" onClick={() => actions.step('+')}>Reservations</Button>
-            </ButtonGroup>
-          </React.Fragment>
-        )
-
-
-      }}
-    </Consumer>
-
-  )
-}
