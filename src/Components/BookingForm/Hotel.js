120a0583f8c17cf4f5997dd3bcc1a63079162c80
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-26 14:35:10 +0800

    Improve style of hotel component

--- src/Components/BookingForm/Hotel.js
+++ src/Components/BookingForm/Hotel.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import Currency from 'react-currency-formatter';
 import numeral from 'numeral';
 
 export default ({tourDates, hotel, adults}) => {
-  let nights = tourDates.nights > 1 ? 'nights' : 'night'
-  let adultPrice = (tourDates.nights * hotel.price) + adults[1]
+  let nights = tourDates.nights > 1 ? 'nights' : 'night';
+  let adultPrice = (tourDates.hotelNights * hotel.price) + adults[1];
+  let price = <Currency quantity={adultPrice} currency="PHP" pattern="!##,### "/>;
+  const format = (price) => `₱${numeral(price).format('0,0')}`
   return (
     <HotelWrapper>
       <Image src={hotel.photo} />
       <div style={{padding: '5px'}}>
         <HotelName style={{textTransform: 'capitalize'}}>{hotel.name.toLowerCase()}</HotelName>
         <p>{`${tourDates.nights} ${nights}`}</p>
-        <PriceLabel>
-          Php {numeral(adultPrice).format('0,0')}/person
-        </PriceLabel>
+        <PriceLabel>{`${format(adults[1])} + (${format(hotel.price)} x ${tourDates.hotelNights} ${nights}) = ${format(adultPrice)}`}</PriceLabel>
       </div>
     </HotelWrapper>
   )
 }
 
 const HotelWrapper = styled.div`
+  font-size: 18px;
   margin: 25px 5px;
   display: grid;
-  grid-template-columns: 115px 1fr;
+  grid-template-columns: 120px 1fr;
   grid-gap: 5px;
   margin-bottom: 15px;
   align-items: top;
@@ -31,7 +33,7 @@ const HotelWrapper = styled.div`
   border: 1px solid green;
 `
 const HotelName = styled.p`
-  font-size: 22px;
+  font-size: 1.2em;
   line-height: 1rem;
   padding-top: 4px;
   margin-bottom:0px;
