8cf80094b9d5b0b7c2ebb244e7828281278e3ab1
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-26 17:23:26 +0800

    Handle reservation form validation and submission

--- src/Components/BookingForm/Context/index.js
+++ src/Components/BookingForm/Context/index.js
@@ -4,16 +4,16 @@ import { addDays, differenceInCalendarDays } from 'date-fns';
 import isEmail from 'validator/lib/isEmail';
 
 import { Calculate } from 'price-compute-js';
-import postReservation  from './postReservation';
+import postReservation from './postReservation';
 
 import { setOptionPaymentDate } from 'tour-dates-utility';
 
 export const BookingContext = React.createContext();
 
-export const Provider = ({data, children}) => {
+export const Provider = ({ data, children }) => {
   const today = new Date();
   const { name, code, slug, type, duration, duration_text, info, responsibilities, terms } = data
-  const {startday, offsetnights} = data;
+  const { startday, offsetnights } = data;
 
   const InitialUserInput = {
     inquiryDate: today,
@@ -33,143 +33,112 @@ export const Provider = ({data, children}) => {
     setOptionPaymentDate(today, tourDates.from).then(date => {
       InitialUserInput.optionDate = date;
     })
-    tourDates.to = addDays(tourDates.from, duration-1);
+    tourDates.to = addDays(tourDates.from, duration - 1);
     tourDates.days = differenceInCalendarDays(tourDates.to, tourDates.from) + 1;
     tourDates.nights = tourDates.days - 1;
     tourDates.hotelNights = tourDates.nights - offsetnights;
     InitialUserInput.tourDates = tourDates;
-    InitialUserInput.hotel = {code:"", price:0, name:"", photo:""};
+    InitialUserInput.hotel = { code: "", price: 0, name: "", photo: "" };
     InitialUserInput.startDate = tourDates.from
   } else {
     InitialUserInput.tourDate = addDays(today, startday);
     setOptionPaymentDate(today, InitialUserInput.tourDate).then(date => {
       InitialUserInput.optionDate = date
-      
-    }) 
-  } 
+
+    })
+  }
 
   const [state, setState] = useState({ data: data, RFValid: false, termsAccepted: false })
-  const [tourPackage] = useState({name, code, slug, type, duration, duration_text, info, responsibilities, terms })
+  const [tourPackage] = useState({ name, code, slug, type, duration, duration_text, info, responsibilities, terms })
   const [userInput, setUserInput] = useState(InitialUserInput)
 
   const [calculations, setCalculations] = useState(Calculate(userInput));
 
   const onPaxSelect = (payload) => {
-    setUserInput({...userInput,...payload})
-    doComputations({...userInput,...payload})
+    setUserInput({ ...userInput, ...payload })
+    doComputations({ ...userInput, ...payload })
   }
 
   const onSelectDate = (tourDate) => {
     setOptionPaymentDate(userInput.inquiryDate, payload).then(optionDate => {
-      setUserInput({...userInput, tourDate, optionDate})
-      doComputations({...userInput, tourDate, optionDate})
+      setUserInput({ ...userInput, tourDate, optionDate })
+      doComputations({ ...userInput, tourDate, optionDate })
     })
-    
+
   }
 
   const onSelectDates = (tourDates) => {
     setOptionPaymentDate(userInput.inquiryDate, tourDates.from).then(optionDate => {
-      setUserInput({...userInput, tourDates, optionDate})
-      doComputations({...userInput, tourDates, optionDate})
+      setUserInput({ ...userInput, tourDates, optionDate })
+      doComputations({ ...userInput, tourDates, optionDate })
     })
-   
+
   }
 
   const onSelectHotel = (payload) => {
     if (payload) {
-      setUserInput({...userInput, hotel:state.data.hotels.find(hotel => hotel.code === payload)})
-      doComputations({...userInput, hotel:state.data.hotels.find(hotel => hotel.code === payload)})
+      setUserInput({ ...userInput, hotel: state.data.hotels.find(hotel => hotel.code === payload) })
+      doComputations({ ...userInput, hotel: state.data.hotels.find(hotel => hotel.code === payload) })
     } else {
-      setUserInput({...userInput, hotel: {code:"", price:0, name:""}})
-      doComputations({...userInput, hotel: {code:"", price:0, name:""}})
+      setUserInput({ ...userInput, hotel: { code: "", price: 0, name: "" } })
+      doComputations({ ...userInput, hotel: { code: "", price: 0, name: "" } })
     }
-    
+
   }
 
-  const handleRFChange = (e) => {
-    let userInput = { ...this.state.userInput }
-    let termsAccepted = this.state.termsAccepted
-    if (e.target.name == "terms") {
-      termsAccepted = !termsAccepted
+  const handleRFChange = ({ target }) => {
+    if (target.name == "terms") {
+      const RFValid = checkRFvalid(userInput, target.checked)
+      setState({ ...state, termsAccepted: target.checked, RFValid })
     } else {
-      userInput[e.target.name] = (e.target.value) 
+      const RFValid = checkRFvalid({ ...userInput, [target.name]: target.value }, state.termsAccepted)
+      setUserInput({ ...userInput, [target.name]: target.value })
+      setState({ ...state, RFValid })
     }
-    
-    let RFValid = this.isvalidRF(userInput, termsAccepted)
-    this.setState({
-      userInput: userInput,
-      termsAccepted: termsAccepted,
-      RFValid: RFValid
-    })
   }
 
-  const isvalidRF = (userInput, termsAccepted) => {
-    // let name = userInput.name
-    // let email = userInput.email
-    // if (name != ""
-    //   && email != ""
-    //   && isEmail(email)
-    //   && termsAccepted) {
-    //   return true
-    // } else {
-    //   return false
-    // }
+  const checkRFvalid = (userInput, termsAccepted) => {
+    const { name, email } = userInput
+    if (name != "" && email != "" && isEmail(email) && termsAccepted) {
+      return true
+    } else {
+      return false
+    }
   }
 
   const submitBooking = () => {
+    console.log(state, userInput)
     // postReservation({
-    //   input: this.state.userInput,
-    //   calculations: this.state.calculations,
-    //   package: this.state.package
+    //   input: userInput,
+    //   calculations: calculations,
+    //   package: package
     // })
-    // this.step('+')
   }
 
   const doComputations = (payload) => {
     setCalculations(Calculate(payload))
   }
 
-  const resetBookingForm = () => {
-    // this.setState({
-    //   currentStep: 0,
-    //   currentModule: '',
-    //   RFValid: false,
-    //   termsAccepted: false,
-    //   userInput: {
-    //     inquiryDate: new Date(),
-    //     adults: this.state.data.price.adults[0],
-    //     kid02: [0, 0],
-    //     kid35: [0, 0],
-    //     kid611: [0, 0],
-    //     name: "",
-    //     email: "",
-    //     contact: "",
-    //     remarks: ""
-    //   },
-    //   calculations: {},
-    // })
-  }
-    return (
-      <BookingContext.Provider value={{
-        data: state.data,
-        prices: state.data.price,
-        RFValid: state.RFValid,
-        termsAccepted: state.termsAccepted,
-        userInput,
-        calculations,
-        actions: {
-          onPaxSelect,
-          onSelectDate,
-          onSelectDates,
-          onSelectHotel,
-          handleRFChange,
-          submitBooking,
-          resetBookingForm
-        }
-      }}>
-        {children}
-      </BookingContext.Provider>
-    )
+  return (
+    <BookingContext.Provider value={{
+      data: state.data,
+      prices: state.data.price,
+      RFValid: state.RFValid,
+      termsAccepted: state.termsAccepted,
+      userInput,
+      calculations,
+      actions: {
+        onPaxSelect,
+        onSelectDate,
+        onSelectDates,
+        onSelectHotel,
+        handleRFChange,
+        submitBooking,
+      }
+    }}>
+      {children}
+    </BookingContext.Provider>
+  )
 
 }
 
