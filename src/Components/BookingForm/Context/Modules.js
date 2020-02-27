33c6d54bee85a74b892547e772320b51caf94557
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-27 15:44:32 +0800

    Delete unused module

--- src/Components/BookingForm/Context/Modules.js
+++
@@ -1,5 +0,0 @@
-export const DayTour = ['NumPax', 'Calendar', 'Computation', 'Reservation', 'Confirmation']
-
-export const MultiDay = ['NumPax', 'DateRange', 'Computation', 'Reservation', 'Confirmation']
-
-export const MultiDayWithHotel = ['NumPax', 'DateRange', 'Hotels', 'Computation', 'Reservation', 'Confirmation']
