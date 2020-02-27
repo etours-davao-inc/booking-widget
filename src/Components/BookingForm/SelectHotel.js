a8fc5a738b74052076515363526f10f9a26f536e
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-28 00:32:21 +0800

    Fix JSX

--- src/Components/BookingForm/SelectHotel.js
+++ src/Components/BookingForm/SelectHotel.js
@@ -6,7 +6,7 @@ const SelectHotel = ({ hotels }) => {
   const { actions } = useContext(BookingContext)
   return (
     <div style={{}}>
-      <label for="select-hotels" style={styles.label}>Select Accommodation</label>
+      <label htmlFor="select-hotels" style={styles.label}>Select Accommodation</label>
       <Select id="select-hotels" style={styles.select} onChange={(e) => actions.onSelectHotel(e.target.value)}>
         <option value={""} key={0}>Tour Only</option>
         {hotels.map(({code, name, price}) => <option key={code} value={code}>{name.toLowerCase()}</option>)}
