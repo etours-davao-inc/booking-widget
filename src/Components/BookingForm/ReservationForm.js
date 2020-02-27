59bcd1792f5309b7591a5983de60c2b03fdbf7d4
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-28 01:02:38 +0800

    Fix ReservationForm

--- src/Components/BookingForm/ReservationForm.js
+++ src/Components/BookingForm/ReservationForm.js
@@ -9,19 +9,19 @@ export default () => {
   return (
     <>
       <h3>Reservation Form</h3>
-      <ReservationFormWrapper>
+      <ReservationFormWrapper onSubmit={() => actions.submitBooking()}>
         <input type="text" name="name" placeholder="Name" value={userInput.name} required="required" onChange={e => actions.handleRFChange(e)} aria-label="name" />
         <input type="email" name="email" placeholder="Email" value={userInput.email} required="required" onChange={e => actions.handleRFChange(e)} aria-label="email address" />
         <input type="text" name="contact" placeholder="Contact Number" value={userInput.contact} onChange={e => actions.handleRFChange(e)} aria-label="contact number" />
         <textarea name="remarks" placeholder="Extra notes" value={userInput.remarks} rows={4} onChange={e => actions.handleRFChange(e)} aria-label="other remarks" />
         <div id="terms_and_conditions">
           <input id="terms_checkbox" name="terms" type="checkbox" required="required" checked={termsAccepted} onChange={e => actions.handleRFChange(e)} />
-          <label htmlFor="terms_checkbox">Yes, I have read and agree with the terms and conditions above.</label>
+          <label htmlFor="terms_checkbox">I have read and agree with the terms and conditions above.</label>
         </div>
+        {RFValid &&
+          <input type="submit" value="Submit Reservation" className="submit" />
+        }
       </ReservationFormWrapper>
-      <ButtonGroup>
-        <Button next disabled={!RFValid} className="BookingFormButtons btn-next" onClick={() => actions.submitBooking()}>Submit</Button>
-      </ButtonGroup>
     </>
   )
 }
@@ -51,10 +51,20 @@ const ReservationFormWrapper = styled.form`
     align-items: center;
   }
 
-  #terms_and_conditions input  {
-    font-size: 1.2em;
+  #terms_and_conditions input[type=checkbox]  {
+    align-self: top;
+    transform: scale(2.5);
     width: auto;   
     padding: 0;
-    margin: 0 10px 0 0; 
+    margin: 0 15px 0 10px; 
+    background-color: white;
+  }
+  .submit {
+    flex-grow: 1;
+    border-radius: 2px;
+    padding: 8px;
+    background-color: #2ecc71;
+    color: white;
+    border: none; 
   }
 `
