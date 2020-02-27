d8d4ed4c994b4380a292acbc7817282a6ce5d953
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-27 15:44:21 +0800

    Move styles to loader

--- src/Components/BookingForm/Loader.js
+++ src/Components/BookingForm/Loader.js
@@ -1,12 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default () => (
-  <div className="responsive-widths my-2 rounded d-flex justify-content-center align-items-center"> 
-    <div className="lds-ellipsis">
+  <div style={{minHeight: '300px', backgroundColor: 'white'}} className="responsive-widths my-2 rounded d-flex justify-content-center align-items-center"> 
+    <Loader>
       <div></div>
       <div></div>
       <div></div>
       <div></div>
-    </div>
+    </Loader>
   </div>
 )
+
+const Loader = styled.div`
+.lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    top: 27px;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background: white;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 6px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 6px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 26px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 45px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(19px, 0);
+    }
+  }
+`
